89270419b716763b3695c08c3ca21548f458cfb4
Zetta Kim <zetta.kim@icloud.com>
2025-06-09 12:44:47 +0900

    6/9 1244

--- ch02/02_is_palindrome.py
+++ ch02/02_is_palindrome.py
@@ -1,21 +1,20 @@
 def is_palindrome(word: str) -> bool:
-    """
-    Check if a word is a palindrome.
-
-    A palindrome reads the same forwards and backwards.
-
-    :param word: The word to check.
-    :return: True if the word is a palindrome, False otherwise.
+    """문자열 word가 회문(palindrome)인지 검사한다.
+    Arguments:
+        word (str): 회문인지 검사할 문자열
+    Return:
+        bool: 회문이면 True, 그렇지 않으면 False를 반환
     """
     left: int = 0
     right: int = len(word)-1
     while left < right:
         if word[left] != word[right]:
             return False
-        left, right = left+1, right-1
+        left, right = left + 1, right - 1
     return True
 
-    # test cases
-    words = ["racecar", "rotor", "tomato", "별똥별", "코끼리", "우영우"]
-    for word in words:
-        print(f"{word} is a palindrome: {is_palindrome(word)}")
+
+# Test code
+words = ["racecar", "rotor", "tomato", "별똥별", "코끼리", "우영우"]
+for word in words:
+    print(f"Is '{word}' palindrome?  {is_palindrome(word)}")
